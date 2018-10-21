780144e8c545a4d05bb6681efa3ded54f419f2e3
Dustin <Dustin.McManus@hotmail.com>
2018-10-21 00:37:26 -0500

    started conditional and lists assignment

--- src/App.js
+++ src/App.js
@@ -1,65 +1,19 @@
-import React, { Component } from "react";
-import UserInput from "./UserInput";
-import UserOutput from "./UserOutput";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
-  state = {
-    username: "Joe"
-  };
-
-  changeUsernameHandler = event => {
-    this.setState({
-      username: event.target.value
-    });
-  };
-
   render() {
     return (
       <div className="App">
         <ol>
-          <li>Create TWO new components: UserInput and UserOutput</li>
-          <li>
-            UserInput should hold an input element, UserOutput two paragraphs
-          </li>
-          <li>
-            Output multiple UserOutput components in the App component (any
-            paragraph texts of your choice)
-          </li>
-          <li>
-            Pass a username (of your choice) to UserOutput via props and display
-            it there
-          </li>
-          <li>
-            Add state to the App component (=> the username) and pass the
-            username to the UserOutput component
-          </li>
-          <li>
-            Add a method to manipulate the state (=> an event-handler method)
-          </li>
-          <li>
-            Pass the event-handler method reference to the UserInput component
-            and bind it to the input-change event
-          </li>
-          <li>
-            Ensure that the new input entered by the user overwrites the old
-            username passed to UserOutput
-          </li>
-          <li>
-            Add two-way-binding to your input (in UserInput) to also display the
-            starting username
-          </li>
-          <li>
-            Add styling of your choice to your components/ elements in the
-            components - both with inline styles and stylesheets
-          </li>
+          <li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
+          <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
+          <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
+          <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
+          <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
+          <li>When you click a CharComponent, it should be removed from the entered text.</li>
         </ol>
-        <UserInput
-          username={this.state.username}
-          changed={this.changeUsernameHandler}
-        />
-        <UserOutput username={this.state.username} />
-        <UserOutput username={this.state.username} />
+        <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
       </div>
     );
   }
